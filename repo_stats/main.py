b46d563fce847fd9de17718ec9a1adb5d3562504
Nathan Pietrantonio <pietrann@uci.edu>
2024-11-12 18:09:52 -0800

    Add command line input support

--- repo_stats/main.py
+++ repo_stats/main.py
@@ -1,8 +1,25 @@
+import argparse
 from utils.stats_generator import StatsGenerator
 
 if __name__ == "__main__":
-    user = input("Enter GitHub Username: ")
-    repo = input("Enter GitHub Repository: ")
+    parser = argparse.ArgumentParser(
+        prog="repo_stats",
+        description="Generate stats for a GitHub repository",
+        epilog="Example: repo_stats user/repo"
+    )
+    parser.add_argument("-u", "--user", nargs=1, help="The GitHub username of the repository owner")
+    parser.add_argument("-r", "--repo", nargs=1, help="The name of the repository")
+    args = parser.parse_args()
+
+    if not args.user:
+        user = input("Enter GitHub Username: ")
+    else:
+        user = args.user
+
+    if not args.repo:
+        repo = input("Enter GitHub Repository: ")
+    else:
+        repo = args.repo
 
     stats = StatsGenerator(repo_name=repo, user_name=user, verbose=True)
 
