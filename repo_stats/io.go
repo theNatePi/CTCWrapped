13bf3f975ba7e34ae99aa347417a45dac692d73d
Nathan Pietrantonio <pietrann@uci.edu>
2025-05-26 18:56:14 -0700

    Chore: Add docstrings

--- repo_stats/io.go
+++ repo_stats/io.go
@@ -23,14 +23,31 @@ var (
 	None      = Color{""}
 )
 
-func getInput(prompt string, promptType Color) string {
-	return getInputAndRespond(prompt, promptType, "", None)
+// Get the under input
+
+// Parameters:
+//
+//   - prompt: The prompt for the user, displayed before input, nothing printed if empty string
+//   - promptColor: The Color of the prompt
+//
+// Returns the user input as a string
+func getInput(prompt string, promptColor Color) string {
+	return getInputAndRespond(prompt, promptColor, "", None)
 }
 
-func getInputAndRespond(prompt string, promptType Color,
+// Get user input and provide a response
+//
+// Parameters:
+//   - prompt: The prompt for the user, displayed before input, nothing printed if empty string
+//   - promptColor: The Color of the prompt
+//   - response: The response to respond after user input, nothing printed if empty string
+//   - responseType: The Color of the response
+//
+// Returns the user input as a string
+func getInputAndRespond(prompt string, promptColor Color,
 	response string, responseType Color) string {
 	if prompt != "" {
-		fmt.Printf("%s%s: %s", promptType, prompt, End)
+		fmt.Printf("%s%s: %s", promptColor, prompt, End)
 	}
 	var input string
 	_, err := fmt.Scanln(&input)
@@ -43,14 +60,27 @@ func getInputAndRespond(prompt string, promptType Color,
 	return input
 }
 
+// Output a message
+//
+// Parameters:
+//   - message: The message to be displayed
+//   - messageColor: The Color of the message
+func ouput(message string, messageColor Color) {
+	outputWithTitle("", None, message, messageColor)
+}
+
+// Output a message with a title
+//
+// Parameters:
+//   - title: The title of the message, displayed above in bold, empty string for no title
+//   - titleColor: The Color of the title
+//   - message: The message to be displayed, displayed below title
+//   - messageColor: The Color of the message
 func outputWithTitle(title string, titleColor Color, message string, messageColor Color) {
+
 	if title != "" {
 		fmt.Printf("\033[1m%s%s%s\n", titleColor, title, End)
 	}
 
 	fmt.Printf("%s%s%s\n", messageColor, message, End)
-}
-
-func ouput(message string, messageColor Color) {
-	outputWithTitle("", None, message, messageColor)
 }
