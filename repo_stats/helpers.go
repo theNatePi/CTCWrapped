a96b9d22fdd264f03a14a187077df21f2d25a7b3
Nathan Pietrantonio <pietrann@uci.edu>
2025-05-28 12:21:30 -0700

    Chore: reformat error response

--- repo_stats/helpers.go
+++ repo_stats/helpers.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func wrapError(err error, function string, context string) error {
-	return fmt.Errorf("In %s: %s\n%s:", function, err, context)
+	return fmt.Errorf("In %s: %s\n%s", function, err, context)
 }
